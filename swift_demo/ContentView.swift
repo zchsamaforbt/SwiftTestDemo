72bb5242e15c513870f8012dbc5d9f9438a591cd
chenhua <zhangchenhua.sam@bytedance.com>
2023-01-30 20:13:48 +0800

    Initial Commit

---
+++ swift_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_demo
+//
+//  Created by ByteDance on 2023/1/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_demo/swift_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_demoApp.swift
+//  swift_demo
+//
+//  Created by ByteDance on 2023/1/30.
+//
+
+import SwiftUI
+
+@main
+struct swift_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
